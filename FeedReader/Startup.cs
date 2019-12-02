08d962285e7269bce46a542a8bea2b4e18ccc90d
Berrysoft <Strawberry_Str@hotmail.com>
2019-12-02 13:17:26 +0800

    Add refresh.

--- FeedReader/Startup.cs
+++ FeedReader/Startup.cs
@@ -1,3 +1,4 @@
+using System.Net.Http;
 using Microsoft.AspNetCore.Components.Builder;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -7,6 +8,7 @@ namespace FeedReader
     {
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSingleton<HttpClient>();
         }
 
         public void Configure(IComponentsApplicationBuilder app)
