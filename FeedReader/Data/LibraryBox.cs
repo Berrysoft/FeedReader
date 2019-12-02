1461da7dbcfee39c2f0167deefbcd52a26d765d1
Berrysoft <Strawberry_Str@hotmail.com>
2019-12-02 23:56:04 +0800

    Add about page.

---
+++ FeedReader/Data/LibraryBox.cs
@@ -0,0 +1,18 @@
+﻿namespace FeedReader.Data
+{
+    public class LibraryBox
+    {
+        public LibraryBox(string name, string? url, string license, string? licenseUrl)
+        {
+            Name = name;
+            Url = url;
+            License = license;
+            LicenseUrl = licenseUrl;
+        }
+
+        public string? Name { get; set; }
+        public string? Url { get; set; }
+        public string? License { get; set; }
+        public string? LicenseUrl { get; set; }
+    }
+}
