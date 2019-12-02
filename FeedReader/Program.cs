d2fc1b4dddebafc8c41a179cb0365fe4040bb996
Berrysoft <Strawberry_Str@hotmail.com>
2019-12-02 11:25:17 +0800

    Finish app framework.

---
+++ FeedReader/Program.cs
@@ -0,0 +1,12 @@
+﻿using WebWindows.Blazor;
+
+namespace FeedReader
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            ComponentsDesktop.Run<Startup>("FeedReader", "wwwroot/index.html");
+        }
+    }
+}
