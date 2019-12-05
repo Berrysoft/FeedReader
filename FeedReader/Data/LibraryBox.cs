d863ba640945db9f40a4f07eae34c559a7e53920
Berrysoft <Strawberry_Str@hotmail.com>
2019-12-05 17:50:27 +0800

    Cleanup.

--- FeedReader/Data/LibraryBox.cs
+++ FeedReader/Data/LibraryBox.cs
@@ -10,9 +10,9 @@
             LicenseUrl = licenseUrl;
         }
 
-        public string? Name { get; set; }
+        public string Name { get; set; }
         public string? Url { get; set; }
-        public string? License { get; set; }
+        public string License { get; set; }
         public string? LicenseUrl { get; set; }
     }
 }
