85972a71a02da5ea5fe041f029e7c17132fcd233
Berrysoft <Strawberry_Str@hotmail.com>
2019-12-06 00:16:46 +0800

    Use shell execute extern link.

---
+++ FeedReader/Shared/ExternLink.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Rendering;
+using Microsoft.AspNetCore.Components.Web;
+
+namespace FeedReader.Shared
+{
+    public class ExternLink : ComponentBase
+    {
+        protected override void BuildRenderTree(RenderTreeBuilder builder)
+        {
+            /*
+             * <span style="cursor: pointer" @onclick="OnClick">
+             *     @ChildContent
+             * </span>
+             */
+            builder.OpenElement(0, Tag);
+            builder.AddMultipleAttributes(1, Microsoft.AspNetCore.Components.CompilerServices.RuntimeHelpers.TypeCheck<IEnumerable<KeyValuePair<string, object>>>(InputAttributes!));
+            if (Uri != null)
+            {
+                builder.AddAttribute(2, "style", "cursor: pointer");
+            }
+            builder.AddAttribute(3, "onclick", EventCallback.Factory.Create<MouseEventArgs>(this, OnElementClick));
+            builder.AddContent(4, ChildContent);
+            builder.CloseElement();
+        }
+
+        [Parameter]
+        public string Tag { get; set; } = "a";
+
+        [Parameter]
+        public RenderFragment? ChildContent { get; set; }
+
+        [Parameter]
+        public string? Uri { get; set; }
+
+        [Parameter]
+        public EventCallback<MouseEventArgs> OnClick { get; set; }
+
+        [Parameter(CaptureUnmatchedValues = true)]
+        public Dictionary<string, object>? InputAttributes { get; set; }
+
+        private Task OnElementClick(MouseEventArgs args)
+        {
+            if (Uri != null)
+            {
+                ProcessStartInfo info = new ProcessStartInfo(Uri);
+                info.UseShellExecute = true;
+                Process.Start(info);
+            }
+            return OnClick.InvokeAsync(args);
+        }
+    }
+}
